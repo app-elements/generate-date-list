d3475df420ced2fcb99938dc3458123bf1c5fdde
Juan Orozco <hi@juanorozco.com>
2017-04-28 23:58:36 -0400

    0.0.5

---
+++ dist/amd/generate-date-list.js
@@ -0,0 +1,20 @@
+/*generate-date-list@0.0.4#generate-date-list*/
+define(['exports'], function (exports) {
+    'use strict';
+    Object.defineProperty(exports, '__esModule', { value: true });
+    exports.default = function (length, incrementor, startDate) {
+        var iterator = generateIterator(incrementor, new Date(startDate));
+        return Array.from(Array(length)).map(iterator);
+    };
+    var generateIterator = exports.generateIterator = function generateIterator(incrementor, startDate) {
+        var lastDate = void 0;
+        return function () {
+            if (lastDate) {
+                lastDate = incrementor(new Date(lastDate));
+            } else {
+                lastDate = startDate;
+            }
+            return lastDate;
+        };
+    };
+});
