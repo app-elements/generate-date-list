ab7214277e865c9425b0982119f2c462be17a73c
Juan Orozco <hi@juanorozco.com>
2017-04-28 23:55:08 -0400

    0.0.4

---
+++ dist/cjs/generate-date-list.js
@@ -0,0 +1,18 @@
+/*generate-date-list@0.0.3#generate-date-list*/
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.default = function (length, incrementor, startDate) {
+    var iterator = generateIterator(incrementor, new Date(startDate));
+    return Array.from(Array(length)).map(iterator);
+};
+var generateIterator = exports.generateIterator = function generateIterator(incrementor, startDate) {
+    var lastDate = void 0;
+    return function () {
+        if (lastDate) {
+            lastDate = incrementor(new Date(lastDate));
+        } else {
+            lastDate = startDate;
+        }
+        return lastDate;
+    };
+};
